18e7a93c03569c002a00881cb896c31db51af04e
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-02-14 10:36:21 -0300

    Gestão de entregadores

---
+++ Anotações.js
@@ -0,0 +1,9 @@
+/**
+ * Anotações para Estudos
+ *
+ * Criar uma migrate com os dados nessesarios depois o respectivél
+ * model e o controller
+ *
+ * Para uplods dos arquivo usaremos => Biblioteca { Multer }
+ *
+ */
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -16,5 +21,13 @@ routes.get('/recipients/:id', RecipientController.show);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 routes.delete('/recipients/:id', RecipientController.delete);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/deliverymans', DeliverymanController.index);
+routes.get('/deliverymans/:id', DeliverymanController.show);
+routes.post('/deliverymans', DeliverymanController.store);
+routes.put('/deliverymans/:id', DeliverymanController.update);
+routes.delete('/deliverymans/:id', DeliverymanController.delete);
 
 export default routes;
