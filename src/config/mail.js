8d635b34242764e033210816e0d43f30bd638c21
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-02-17 10:13:00 -0300

    cadastro de entrega e envido da encomenda por email

---
+++ src/config/mail.js
@@ -0,0 +1,23 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '5f3041c347489a',
+    pass: 'b25e4f19b3ddce',
+  },
+  default: {
+    from: 'Maya software <noreply@Fastfeet.com>',
+  },
+};
+
+/**
+ * Serviços de email
+ * => Amazon SES
+ * => Mailgun
+ * => Sparkpost
+ * => Mandril ( Mailchimp )
+ *
+ * Só funciona para desenvolvimento
+ * => Mailtrap (DEV)
+ */
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import FileController from './app/controllers/FileController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import DeriveryController from './app/controllers/DeriveryController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -29,5 +30,7 @@ routes.get('/deliverymans/:id', DeliverymanController.show);
 routes.post('/deliverymans', DeliverymanController.store);
 routes.put('/deliverymans/:id', DeliverymanController.update);
 routes.delete('/deliverymans/:id', DeliverymanController.delete);
+
+routes.post('/delivery', DeriveryController.store);
 
 export default routes;
