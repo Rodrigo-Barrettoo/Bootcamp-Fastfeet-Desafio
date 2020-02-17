3b160062ea21390f7ba92f20e2c620ca3e01727f
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-02-17 14:14:23 -0300

    ajuste

---
+++ src/app/controllers/DeliveryStartController.js
@@ -0,0 +1,112 @@
+import * as Yup from 'yup';
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
+
+class DeliveryStartController {
+  async show(req, res) {
+    const deliveryman = await Deliveryman.findByPk(req.params.id);
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Deliveryman already exists' });
+    }
+
+    const { name, email, avatar_id } = deliveryman;
+
+    return res.json({ name, email, avatar_id });
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const deliverymanExists = await Deliveryman.findOne({
+      where: { name: req.body.name },
+    });
+
+    if (deliverymanExists) {
+      return res.status(400).json({ error: 'Deliveryman already exists' });
+    }
+
+    const { id, name, email } = await Deliveryman.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      avatar_id: Yup.number().positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const deliveryman = await Deliveryman.findByPk(req.params.id);
+
+    if (!deliveryman) {
+      return res.status(401).json({ error: 'Deliveryman does not exist' });
+    }
+
+    const { avatar_id, email } = await req.body;
+
+    if (avatar_id) {
+      const avatar = await File.findOne({
+        where: { id: avatar_id },
+      });
+
+      if (!avatar) {
+        return res.status(400).json('Avatar not found');
+      }
+    }
+
+    if (email && email !== deliveryman.email) {
+      const deliverymanExists = await Deliveryman.findOne({ where: { email } });
+
+      if (deliverymanExists) {
+        return res.status(400).json({ error: 'Email already registered' });
+      }
+    }
+
+    const { name } = await deliveryman.update(req.body);
+
+    const avatar = deliveryman.avatar_id;
+
+    return res.json({ email, name, avatar });
+  }
+
+  async delete(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number()
+        .positive()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.params))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const deliveryman = await Deliveryman.findByPk(req.params.id);
+
+    if (!deliveryman) {
+      return res.status(401).json({ error: 'Deliveryman does not exist' });
+    }
+
+    deliveryman.destroy({ where: Deliveryman });
+
+    return res.status(200).json({ message: 'Deliveryman has been deleted' });
+  }
+}
+
+export default new DeliveryStartController();
